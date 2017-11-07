926ba6b2f57e2ebf0354b3c67f7bd743ef7e54c6
Blakeley Bond <burrick2003@yahoo.com>
2017-11-07 02:43:44 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,13 @@
 def starts_with_a_vowel?(word)
-
+vowel_output = []
+cons_output = []
+vowel_output << word.scan(/^[aeiouAEIOU]/)
+cons_output << word.scan(/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ])
+  if vowel_output != []
+  true
+  elsif cons_output != []
+  false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
